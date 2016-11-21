43461d72eb8cb0e61c4e4548427f6e98edfa5e10
Stunt <nior_cs@hotmail.com>
2016-11-21 19:21:20 +0700

    Add bot file
    Signed-off-by: Stunt <nior_cs@hotmail.com>

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
   $csc = '5dba7b5e31d22c110ae84c75f82b90f4';
   $token = 'Yh9WBt5/mhi9VnFR3EPckw23Bd5hnm3zinl+danH68ntVDqA/LUOwAprQf3lZQEoYkNZMng7Hdaw5OLD+lTb2xjIAYp2EIVPtuZj8D4B4Au3JWklZSJ50Rlcen5jc3JqaTJci5ZLCTuY3RAPZ5ZK6wdB04t89/1O/w1cDnyilFU=';
 
+  /*{"channelId":1489175380,"mid":"u003f73c9c2aa2630a5cb30655952bb57"}  */
+
   $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($token);
   $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $csc]);
 
